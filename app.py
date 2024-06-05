4f5b6fe92aa0c0ba11a6f2e49d1e79d73ff5cb9f
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-06-05 12:50:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,127 @@
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.utilities import SQLDatabase
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
+import streamlit as st
+import time
+
+def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
+    db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+    return SQLDatabase.from_uri(db_uri)
+
+def get_sql_chain(db):
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into account.
+    
+    <SCHEMA>{schema}</SCHEMA>
+    
+    Conversation History: {chat_history}
+    
+    Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
+    
+    For example:
+    Question: which 3 artists have the most tracks?
+    SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+    Question: Name 10 artists
+    SQL Query: SELECT Name FROM Artist LIMIT 10;
+    
+    Your turn:
+    
+    Question: {question}
+    SQL Query:
+    """
+    
+    prompt = ChatPromptTemplate.from_template(template)
+  
+    llm = ChatOpenAI(model="gpt-4o")
+    #llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
+  
+    def get_schema(_):
+        return db.get_table_info()
+  
+    return (
+        RunnablePassthrough.assign(schema=get_schema)
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    
+def get_response(user_query: str, db: SQLDatabase, chat_history: list):
+    sql_chain = get_sql_chain(db)
+  
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, question, sql query, and sql response, write a natural language response.
+    <SCHEMA>{schema}</SCHEMA>
+
+    Conversation History: {chat_history}
+    SQL Query: <SQL>{query}</SQL>
+    User question: {question}
+    SQL Response: {response}"""
+  
+    prompt = ChatPromptTemplate.from_template(template)
+  
+    llm = ChatOpenAI(model="gpt-4o")
+    #llm = ChatGroq(model="mixtral-8x7b-32768", temperature=0)
+  
+    chain = (
+        RunnablePassthrough.assign(query=sql_chain).assign(
+            schema=lambda _: db.get_table_info(),
+            response=lambda vars: db.run(vars["query"]),
+        )
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+  
+    return chain.invoke({
+        "question": user_query,
+        "chat_history": chat_history,
+    })
+    
+  
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="¡Hola! Soy tu asistente de SQL. Pregúntame cualquier cosa sobre tu base de datos."),
+    ]
+
+load_dotenv()
+
+st.set_page_config(page_title="Chat Origen Medios", page_icon=":speech_balloon:")
+
+st.title("Chat IA")
+
+# Conexión automática a la base de datos
+db = init_database("doadmin", "AVNS_Vfn884XHZhbBRmGnXLo", "chatgpt-do-user-3243287-0.c.db.ondigitalocean.com", "25060", "OrigenMedios")
+st.session_state.db = db
+st.success("Conectado a OrigenMedios DDBB!")
+
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
+
+user_query = st.chat_input("Escribe tu Pregunta...")
+if user_query is not None and user_query.strip() != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+        
+    # Mostrar un mensaje de "pensando"
+    with st.chat_message("AI"):
+        with st.spinner(text='Esperando respuesta...'):
+            time.sleep(2)  # Simular un tiempo de espera mientras se procesa la respuesta
+            response = get_response(user_query, st.session_state.db, st.session_state.chat_history)
+            st.markdown(response)
+            
+    st.session_state.chat_history.append(AIMessage(content=response))
 
